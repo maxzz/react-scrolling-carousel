ec8f01b629a988d7f9f898179a52af8b291e733c
Max Zakharzhevskiy <maxzz@msn.com>
2021-03-09 23:09:32 -0800

    Feature: vite config custom plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+import fs from 'fs';
+import path from 'path';
+
+function copyExtra() {
+    return {
+        name: 'copy-extra',
+        async buildEnd() {
+            let indexHtml = path.join(__dirname, 'dist', 'index.html');
+            let cnt = fs.readFileSync(indexHtml).toString();
+            console.log('\n\n--------------\n', indexHtml, cnt);
+        }
+    };
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+    plugins: [
+        reactRefresh(),
+        copyExtra()
+    ]
 })
